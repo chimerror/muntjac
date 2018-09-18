5f650893628a1adc36d1a86366db6dbac662a29f
Jason Mitchell <foobardog@hotmail.com>
2018-09-18 06:31:13 -0700

    Mouse Moving

--- Assets/Characters/sammy/PlayerController.cs
+++ Assets/Characters/sammy/PlayerController.cs
@@ -11,41 +11,71 @@ public class PlayerController : MonoBehaviour
     private float verticalSpeed = 10f;
 
     [SerializeField]
+    private float mouseMoveSpeed = 10f;
+
+    [SerializeField]
     private float raycastDistance = 25f;
 
-    private BoxCollider2D collider;
+    private BoxCollider2D playerCollider;
     private bool stopRight;
     private bool stopLeft;
     private bool stopUp;
     private bool stopDown;
+    private bool isMouseMoving;
+    private Vector2 mouseMoveTarget;
 
     private void Awake()
     {
-        collider = GetComponent<BoxCollider2D>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         Vector3 currentPosition = transform.position;
         float desiredHorizontal = Input.GetAxis("Horizontal");
-        if ((desiredHorizontal > 0 && !stopRight) || (desiredHorizontal < 0 && !stopLeft))
-        {
-            currentPosition.x += desiredHorizontal * horizontalSpeed;
-        }
         float desiredVertical = Input.GetAxis("Vertical");
-        if ((desiredVertical > 0 && !stopUp) || (desiredVertical < 0 && !stopDown))
+
+        if (isMouseMoving && (desiredHorizontal != 0 || desiredVertical != 0))
         {
-            currentPosition.y += desiredVertical * verticalSpeed;
+            isMouseMoving = false;
         }
-        transform.position = currentPosition;
+        else if(Input.GetMouseButtonDown(0))
+        {
+            isMouseMoving = true;
+            mouseMoveTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (!isMouseMoving)
+        {
+            if ((desiredHorizontal > 0 && !stopRight) || (desiredHorizontal < 0 && !stopLeft))
+            {
+                currentPosition.x += desiredHorizontal * horizontalSpeed;
+            }
+            if ((desiredVertical > 0 && !stopUp) || (desiredVertical < 0 && !stopDown))
+            {
+                currentPosition.y += desiredVertical * verticalSpeed;
+            }
+            transform.position = currentPosition;
+        }
+        else if (isMouseMoving && (Vector2)transform.position != mouseMoveTarget)
+        {
+            if (stopRight || stopLeft || stopUp || stopDown)
+            {
+                isMouseMoving = false;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, mouseMoveTarget, mouseMoveSpeed);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         RaycastHit2D[] results = new RaycastHit2D[5];
-        stopRight = collider.Raycast(Vector2.right, results, raycastDistance) > 0;
-        stopLeft = collider.Raycast(Vector2.left, results, raycastDistance) > 0;
-        stopUp = collider.Raycast(Vector2.up, results, raycastDistance) > 0;
-        stopDown = collider.Raycast(Vector2.down, results, raycastDistance) > 0;
+        stopRight = playerCollider.Raycast(Vector2.right, results, raycastDistance) > 0;
+        stopLeft = playerCollider.Raycast(Vector2.left, results, raycastDistance) > 0;
+        stopUp = playerCollider.Raycast(Vector2.up, results, raycastDistance) > 0;
+        stopDown = playerCollider.Raycast(Vector2.down, results, raycastDistance) > 0;
     }
 }
