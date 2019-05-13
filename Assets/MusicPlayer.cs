c3a8253845d9e4a235b87ea87a1c3d846ecd946b
Jason Mitchell <foobardog@hotmail.com>
2019-05-12 17:25:51 -0700

    Adding Don's Music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource source;
+
+    public void PlayMusic()
+    {
+        GameObject.FindGameObjectWithTag("music");
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+    }
+}
